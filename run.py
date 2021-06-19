bc485c914170bab10b0be943ca757453d1e4ac99
Roomacarthur <roomacarthur@gmail.com>
2021-06-19 21:49:17 +0100

    added start_quiz function for initiating the start of the quiz

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 import time
 import sys
 
+
+# Typing animation function.
 def typing(text):
     """
     This is a modified print function, rather than the whole print statement appearing all at once, the text has a timing in-between characters. In turn this provides a typing like animation. 
@@ -11,5 +13,15 @@ def typing(text):
         sys.stdout.flush()
         time.sleep(0.08)
 
+typing("Typing animation test.\n")
 
-typing("Typing animation test.")
+def start_game():
+    """
+    Starts the game, retrieves username from player and welcomes them to the game. 
+    """
+    # ---WELCOME MESSAGE
+    p_name = input("Please enter a username: \n")
+    inv_slots = 2
+    typing(f"Welcome {p_name}, good luck and remember you only have {inv_slots} bag spaces!\n")
+
+start_game()
