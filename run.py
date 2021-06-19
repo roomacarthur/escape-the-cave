fe4e8ac1ea3165f320228aa7c28f2c22a747459c
Roomacarthur <roomacarthur@gmail.com>
2021-06-19 21:51:06 +0100

    updated the function 'typing' to take an aditional argument of speed, alows for speed modification on each call

--- run.py
+++ run.py
@@ -4,17 +4,18 @@ import sys
 
 
 # Typing animation function.
-def typing(text):
+def typing(text, speed):
     """
     This is a modified print function, rather than the whole print statement appearing all at once, the text has a timing in-between characters. In turn this provides a typing like animation. 
+    text = The text you wish to enter.
+    speed = The typing animations speed.
     """
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(0.08)
+        time.sleep(speed)
 
-typing("Typing animation test.\n")
-
+# Start quiz function.
 def start_game():
     """
     Starts the game, retrieves username from player and welcomes them to the game. 
@@ -22,6 +23,6 @@ def start_game():
     # ---WELCOME MESSAGE
     p_name = input("Please enter a username: \n")
     inv_slots = 2
-    typing(f"Welcome {p_name}, good luck and remember you only have {inv_slots} bag spaces!\n")
+    typing(f"Welcome {p_name}, good luck and remember you only have {inv_slots} bag spaces!\n", 0.03)
 
 start_game()
